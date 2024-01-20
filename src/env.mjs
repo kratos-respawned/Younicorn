1b04696f487331df485de07aa5444ba01b68a3f1
Gaurav Bhandari <sbhandari1907@protonmail.com>
2024-01-20 14:59:14 +0530

    Add get-port package, update dependencies, and implement function for generating nginx config

--- src/env.mjs
+++ src/env.mjs
@@ -7,8 +7,9 @@ export const env = createEnv({
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
     NEXTAUTH_SECRET: z.string().min(1),
-    ADMIN_MAIL:z.string().email().min(1),
-    ADMIN_PASS:z.string().min(8)
+    ADMIN_MAIL: z.string().email().min(1),
+    ADMIN_PASS: z.string().min(8),
+    DOMAIN: z.string().url().min(1),
   },
   client: {},
   experimental__runtimeEnv: {},
