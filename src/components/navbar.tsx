0ed5e1094e63ddd47b9a1c1f519dc4a08edaca65
Gaurav Bhandari <sbhandari1907@protonmail.com>
2024-01-21 09:00:46 +0530

    add github link and blog page url

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import { buttonVariants } from "@components/ui/button";
 import { UserProfile } from "@components/user-profile";
 
 import { useSession } from "next-auth/react";
+import { Github } from "lucide-react";
 
 export const Navbar = () => {
   const { data: session } = useSession();
@@ -13,13 +14,34 @@ export const Navbar = () => {
       <Link href="/" className="font-cal text-xl md:text-2xl">
         <span className="text-primary font-bold">You</span>nicorn
       </Link>
-      {session ? (
-        <UserProfile user={session.user} />
-      ) : (
-        <Link href="/login" className={cn(buttonVariants())}>
-          Sign In
+      <div className="flex gap-6 items-center">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/kratos-respawned/Younicorn"
+          className={buttonVariants({
+            size: "icon",
+            variant: "ghost",
+          })}
+        >
+          <Github />
+        </a>
+        <Link
+          href="/blogs"
+          className={buttonVariants({
+            variant: "ghost",
+          })}
+        >
+          Blog
         </Link>
-      )}
+        {session ? (
+          <UserProfile user={session.user} />
+        ) : (
+          <Link href="/login" className={cn(buttonVariants())}>
+            Sign In
+          </Link>
+        )}
+      </div>
     </header>
   );
 };
